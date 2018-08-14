7ced70c042b7aa3dfa3ea2f4297367b4bf4a3436
yunshengji <423450928@qq.com>
2018-08-14 15:54:32 +0800

    the backend done with no robust

--- app/views/api/v1/users/index.json.jbuilder
+++ app/views/api/v1/users/index.json.jbuilder
@@ -2,5 +2,6 @@
 json.users do
     json.array! @users do |user|
     json.extract! user, :id, :nickname, :avatar_url, :gender
+    json.user_loveline user.loveline, :id
       end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Loveline.create(user_one_id: 1, user_two_id: 2)
+Loveline.create(user_one_id: 3, user_two_id: 4)
+
+User.create(nickname: "yunshengji", gender: 1, avatar_url: "http//gee", loveline_id: 1)
+User.create(nickname: "Chris", gender: 1, avatar_url: "http//chris", loveline_id: 1)
+User.create(nickname: "Fabio", gender: 1, avatar_url: "http//Fabio", loveline_id: 2)
+User.create(nickname: "Callum", gender: 2, avatar_url: "http//Callum", loveline_id:2)
+
+
+
+a = Post.create(title: "good" ,content: "Today is a good day", user_id: 1, loveline_id: 1)
+b = Post.create(title: "somehow good",content: "Tomorrow will be a good day", user_id: 2, loveline_id: 1)
+c = Post.create(title: "very good",content: "The day after tmr will be a good day",user_id: 1, loveline_id: 1)
