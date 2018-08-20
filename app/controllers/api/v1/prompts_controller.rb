d0835ffee9620f6c7ca5f07fab7363ee7b99c589
yunshengji <423450928@qq.com>
2018-08-20 21:43:25 +0800

    hahaah

--- app/controllers/api/v1/prompts_controller.rb
+++ app/controllers/api/v1/prompts_controller.rb
@@ -1,10 +1,11 @@
 class Api::V1::PromptsController < Api::V1::BaseController
-  before_action :set_prompt, only: [ :destroy ]
+  before_action :set_prompt, only: [ :show, :destroy ]
 
   def index
     @prompts = Prompt.all.order('created_at DESC')
   end
-
+  def show
+  end
 
   def create
     @prompt = Prompt.new(prompt_params)
